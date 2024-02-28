1e9c081f92c60a2ca7eab5e1c6c654df548f8912
CaptainPig-gy <100899925+CaptainPig-gy@users.noreply.github.com>
2024-02-28 13:27:01 -0800

    Comments added

--- gears.py
+++ gears.py
@@ -4,14 +4,15 @@ import matplotlib.pyplot as plt
 # gear ratios from https://www.automobile-catalog.com/auta_details1.php#gsc.tab=0
 # tire circumference is 1.99334 meters for a honda civic
 
-RPM_X = [] # new empty list
+RPM_X = [] # empty list for RPM values
 starting_RPM = 1000 # data from manufacturer starts here
 
+# fills list with values from 1000 - 6700
 while starting_RPM <= 6700:
     RPM_X.append(starting_RPM)
     starting_RPM += 100
 
-# formula for tire rpm and car speed
+# formula for tire rpm and car speed #
 # Engine rpm / transmission gear /axle gear(final drive) = tire rpm
 # Tire circumference in meters x tire rpm = meters per minute
 # Meters per minute x 0.03728283 = mph
@@ -20,22 +21,27 @@ while starting_RPM <= 6700:
 gearRatios = [3.142, 1.869, 1.235, 0.948, 0.727]
 finalDrive = 4.294
 circumference = 1.99334
-finalSpeeds = []
+finalSpeeds = [] # values will be used to graph later
 # tireRPM = 
 
+# goes through each ratio and rpm to find final speed
 for gears in gearRatios:
     # print(gears)
-    speed = []
+    speed = [] # temporary list for each ratio 
     for RPM in RPM_X:
         tireRPM = RPM / gears / finalDrive
         metersPerMinute = circumference * tireRPM
+
+        # appends final speeds to list for this ratio
         speed.append(round(metersPerMinute * 0.03728283, 2))
+    
+    # appends the full list from above to the final list that stores all values
     finalSpeeds.append(speed)
 
-newFile = "speed_data.txt"
+newFile = "speed_data.txt" # creates new file to output all speeds for user
 
 gearNumber = 1
-with open(newFile, "w") as file:
+with open(newFile, "w") as file: # writes data to file
     file.write("RPM Values Tested \n")
     file.write(str(RPM_X) + "\n\n")
     for lists in finalSpeeds:
@@ -44,11 +50,12 @@ with open(newFile, "w") as file:
         file.write(str(lists) + "\n\n")
 
 gearNumber = 1
-for mph in finalSpeeds:
+for mph in finalSpeeds: # graph is plotted
     plt.plot(RPM_X, mph, label = f"Gear {gearNumber}")
     gearNumber += 1
 plt.grid(True)
 plt.xlabel("RPM")
 plt.ylabel("MPH")
+plt.title("Maximum speed of the Gears")
 plt.legend()
 plt.show()
